5ed03f9481085a60dc5b261fa26d093c3649ff05
BottleOfErie <Bottle_of_Erie@outlook.com>
2024-12-13 04:19:47 +0800

    backend begin

---
+++ Backend/OFIndexBackEnd/src/main/java/ikklos/ofindexbackend/OfIndexBackEndApplication.java
@@ -0,0 +1,13 @@
+package ikklos.ofindexbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfIndexBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OfIndexBackEndApplication.class, args);
+    }
+
+}
---
+++ Backend/OFIndexBackEnd/src/test/java/ikklos/ofindexbackend/OfIndexBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package ikklos.ofindexbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfIndexBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
