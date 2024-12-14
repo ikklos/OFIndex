e6d7febf744a89f2063a629287c725bed8213388
ikklos <ikklos@qq.com>
2024-12-15 01:40:43 +0800

    登陆注册页面，主页面探索页面（部分完成）

--- ofindex-frontend/src/components/index/Index.vue
+++ ofindex-frontend/src/components/index/Index.vue
@@ -1,21 +1,55 @@
 <script setup>
 
-import {Avatar} from "@element-plus/icons-vue";
+import {Avatar, CoffeeCup, Reading, Search, Upload} from "@element-plus/icons-vue";
+import {ref} from 'vue'
+import {useRoute} from 'vue-router'
+const HelloText = ref('hello');
+const route = useRoute();
+const RouteName = route.name;
+const IsAdmin = ref(false)
 </script>
 
 <template>
   <div class="index center-layout-column">
     <el-container>
-      <el-header>
-        <el-row :gutter=25>
+      <el-header height="8vh">
+        <el-row :gutter="10">
           <el-col :span="2">
             <div class="avatar-left full-fix">
               <el-avatar size="large"></el-avatar>
             </div>
           </el-col>
-          <el-col :span="1" :offset="18"></el-col>
-          <el-col :span="1"></el-col>
-          <el-col :span="1"></el-col>
+          <el-col :span="4" :offset="8">
+            <div class="text-center">{{HelloText}}</div>
+          </el-col>
+          <el-col :span="2" :offset="2">
+            <div class="button-area center-layout-row">
+              <el-button icon="Upload" type="primary" v-if="IsAdmin.value">
+                上传
+              </el-button>
+            </div>
+          </el-col>
+          <el-col :span="2" >
+            <div class="button-area center-layout-row">
+              <el-button icon="Search" type="primary" :disabled="RouteName === 'r-index-explore'">
+                探索
+              </el-button>
+            </div>
+          </el-col>
+          <el-col :span="2">
+            <div class="button-area center-layout-row">
+              <el-button icon="Reading" type="primary">
+                书架
+              </el-button>
+            </div>
+          </el-col>
+          <el-col :span="2">
+            <div class="button-area center-layout-row">
+              <el-button icon="CoffeeCup" type="primary">
+                社区
+              </el-button>
+            </div>
+          </el-col>
         </el-row>
       </el-header>
       <RouterView></RouterView>
@@ -34,13 +68,14 @@ import {Avatar} from "@element-plus/icons-vue";
   background: #FFFFFF;
 }
 .el-header{
+  position: sticky;
   background-color: #AAAAAA;
-  height:8vh;
 }
-.el-row{
+
+.el-col{
   height: 100%;
 }
-.el-col{
+.el-row{
   height: 100%;
 }
 .avatar-left{
@@ -49,5 +84,18 @@ import {Avatar} from "@element-plus/icons-vue";
   align-items: flex-start;
   text-align: center;
   flex-direction: column;
+}
+.text-center{
+  height: 8vh;
+  text-align: center;
+  line-height: 8vh;
+}
+.button-area{
+  height: 100%;
+  width: 100%;
+}
+.el-button{
+  width: 5vw;
+  height: 4vh;
 }
 </style>
--- ofindex-frontend/src/components/index/IndexExplore.vue
+++ ofindex-frontend/src/components/index/IndexExplore.vue
@@ -1,11 +1,130 @@
 <script setup>
+import {ref,reactive} from "vue";
+let BatchSize = 20;
+const ClassList = reactive([
+  {
+    name: "全部",
+    classId: 0,
+  },{
+    name: "小说",
+    classId: 1,
+  }
+]);
+const NowClass = ref(0);
+const SearchString = ref("");
+const BookItems = ref([]);
+//总共的查询结果数量
+const TotalCount = ref(0);
+//目前已加载的页数
+const LoadedPages = ref(0);
+//发送带查询字符串的查询请求
+let FilterByString = function (Page) {
+    //清空数组
+    TotalCount.value = LoadedPages.value = 0;
+    BookItems.value.splice(0, BookItems.value.length);
+    //获取查询结果
+    let result={
+      totalResult:1000,
+      data:[],
+    };
+    for(let i = 0; i < 20; i++){
+      result.data.push({
+        name: "支柱霞：血本无归",
+        author: "last炫、",
+        description: "S6第一个王者的含金量  我！是！梓！神！ wtm顶死你~",
+        cover: "https://smms.app/delete/KBgrZeLW7w6ua5MzUFIEk2pj4G",
+        tag:["搞笑","炫狗","皮套"],
+      });
+    }
+    //更新结果数量
+    TotalCount.value = result.totalResult;
+    LoadedPages.value = 1;
 
+}
+let ReqForMore = function (Page) {
+  if(LoadedPages.value * BatchSize < TotalCount.value){
+    
+  }
+}
 </script>
 
 <template>
+  <el-container>
+    <el-aside>
+      <el-scrollbar>
+        <el-menu default-active="0">
+          <el-menu-item v-for="{name,classId} in ClassList" :key="classId" :index="String(classId)">
+            {{name}}
+          </el-menu-item>
+        </el-menu>
+      </el-scrollbar>
+    </el-aside>
+    <el-main style="background: #292929" class="center-layout-column">
+      <div class="div-main">
+        <el-row>
+          <el-col :span="16" :offset="4">
+            <el-input v-model="SearchString" prefix-icon="Search" @keyup.enter="FilterByString">
+            </el-input>
+          </el-col>
+        </el-row>
 
+      </div>
+    </el-main>
+  </el-container>
 </template>
 
 <style scoped>
+.el-aside{
+  width: 12vw;
+}
+.el-scrollbar{
+  background: gray;
+}
+.el-menu-item {
+  width: 100%;
+  color: #409EFF !important;
+  i{
+    color: #fff!important;
+  }
+  font-size: large;
+  justify-content: center
+}
 
+.el-menu-item:hover {
+  outline: 0 !important;
+  color: #409EFF !important;
+}
+
+.el-menu-item.is-active {
+  color: #fff !important;
+  background: #409EFF !important;
+}
+
+
+:deep(.el-scrollbar__wrap){
+  overflow: visible !important;
+}
+
+.el-submenu :deep(.el-submenu__title)  {
+  color: #fff !important;
+}
+
+.el-submenu :deep(.el-submenu__title)  i {
+  color: #fff !important;
+}
+
+.el-submenu :deep(.el-menu-item)  {
+  color: #fff !important;
+}
+.el-main{
+  padding: 0 10vw 0 10vw;
+}
+.div-main{
+  width: 100%;
+  height: 100%;
+  background: #FFFFFF;
+}
+.el-input{
+  height: 40px;
+}
 </style>
