3a6003a67110310bb8324187fe78428d39aca23a
saya <772683180ao@gmail.com>
2025-01-05 16:47:01 +0800

    move ip to Config.kt

---
+++ Android/app/src/main/java/site/sayaz/ofindex/data/remote/Config.kt
@@ -0,0 +1,13 @@
+package site.sayaz.ofindex.data.remote
+
+object Config{
+    // COS配置
+    const val SECRET_ID = "AKIDADAtOegT1KPn8GAqOltnTu67wZ0FH7lK"
+    const val SECRET_KEY = "RefT5VhDappb2bzAtVu65uVu8B0c2HME"
+    const val BUCKET_NAME = "ofindex-1318773774"
+    const val REGION = "ap-beijing"
+
+    //private const val BASE_URL = "http://10.194.189.228:8080"
+    const val BASE_URL = "http://175.178.5.83:8080"
+
+}
